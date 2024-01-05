40c01e615aec2a2cfb95cbc6c50c0aa318d3afaf
Matt Gallucci <mattgallucci97@gmail.com>
2024-01-05 13:21:01 -0800

    removed some unused code, kept some in comments for examples

--- handlers/request.go
+++ handlers/request.go
@@ -1,84 +1,84 @@
 package handlers
 
-import (
-	"github.com/gofiber/fiber/v2"
-	"github.com/mattg1243/sqlc-fiber/db"
-)
+// import (
+// 	"github.com/gofiber/fiber/v2"
+// 	"github.com/mattg1243/sqlc-fiber/db"
+// )
 
 // user requests
-type createUserRequest struct {
-	User struct {
-		Username string `json:"username" validate:"required"`
-		Password string `json:"password" validate:"required"`
-		Email string `json:"email" validate:"required,email"`
-		Balance int32 `json:"balance" validate:"required,gte=0"`
-	} `json:"user"`
-}
+// type createUserRequest struct {
+// 	User struct {
+// 		Username string `json:"username" validate:"required"`
+// 		Password string `json:"password" validate:"required"`
+// 		Email string `json:"email" validate:"required,email"`
+// 		Balance int32 `json:"balance" validate:"required,gte=0"`
+// 	} `json:"user"`
+// }
 
-func (r *createUserRequest) bind(c *fiber.Ctx, u *db.User, v *Validator) error {
-	// validate
-	if err := c.BodyParser(r); err != nil {
-		return err
-	}
+// func (r *createUserRequest) bind(c *fiber.Ctx, u *db.User, v *Validator) error {
+// 	// validate
+// 	if err := c.BodyParser(r); err != nil {
+// 		return err
+// 	}
 
-	if err := v.Validate(r); err != nil {
-		return err
-	}
+// 	if err := v.Validate(r); err != nil {
+// 		return err
+// 	}
 
-	u.Username = r.User.Username
-	u.Email = r.User.Email
-	u.Balance = r.User.Balance
-	// hash password
-	h, err := u.HashPassword(r.User.Password)
-	if err != nil {
-		return err
-	}
-	u.Hash = h
+// 	u.Username = r.User.Username
+// 	u.Email = r.User.Email
+// 	u.Balance = r.User.Balance
+// 	// hash password
+// 	h, err := u.HashPassword(r.User.Password)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	u.Hash = h
 
-	return nil
-}
+// 	return nil
+// }
 
-type updateUserRequest struct {
-	User struct {
-		Username string `json:"username" validate:"required"`
-		Email string `json:"email" validate:"required,email"`
-		Balance int32 `json:"balance" validate:"required,gte=0"`
-	}
-}
+// type updateUserRequest struct {
+// 	User struct {
+// 		Username string `json:"username" validate:"required"`
+// 		Email string `json:"email" validate:"required,email"`
+// 		Balance int32 `json:"balance" validate:"required,gte=0"`
+// 	}
+// }
 
-func (r *updateUserRequest) bind(c *fiber.Ctx, u *db.User, v *Validator) error {
-	// validate
-	if err := c.BodyParser(r); err != nil {
-		return err
-	}
+// func (r *updateUserRequest) bind(c *fiber.Ctx, u *db.User, v *Validator) error {
+// 	// validate
+// 	if err := c.BodyParser(r); err != nil {
+// 		return err
+// 	}
 
-	if err := v.Validate(r); err != nil {
-		return err
-	}
+// 	if err := v.Validate(r); err != nil {
+// 		return err
+// 	}
 
-	u.Username = r.User.Username
-	u.Email = r.User.Email
-	u.Balance = r.User.Balance
+// 	u.Username = r.User.Username
+// 	u.Email = r.User.Email
+// 	u.Balance = r.User.Balance
 
-	return nil
-}
+// 	return nil
+// }
 
-type loginUserRequest struct {
-	Username string `json:"username" validate:"required"`
-	Password string `json:"password" validate:"required"`
-}
+// type loginUserRequest struct {
+// 	Username string `json:"username" validate:"required"`
+// 	Password string `json:"password" validate:"required"`
+// }
 
-func (r *loginUserRequest) bind(c *fiber.Ctx, v *Validator) error {
-	if err :=c.BodyParser(r); err != nil {
-		return err
-	}
+// func (r *loginUserRequest) bind(c *fiber.Ctx, v *Validator) error {
+// 	if err :=c.BodyParser(r); err != nil {
+// 		return err
+// 	}
 
-	if err := v.Validate(r); err != nil {
-		return err
-	}
+// 	if err := v.Validate(r); err != nil {
+// 		return err
+// 	}
 
-	return nil
-}
+// 	return nil
+// }
 
 // TODO create request structs and bind funcs for the routes corresponding to the other models
 
--- routes/routes.go
+++ routes/routes.go
@@ -2,22 +2,20 @@ package routes
 
 import (
 	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/keyauth"
 	"github.com/mattg1243/sqlc-fiber/handlers"
-	"github.com/mattg1243/sqlc-fiber/middleware"
 )
 
 func LoadRoutes(a *fiber.App, h *handlers.Handler) {
 	// root routes
 	a.Get("/", h.GetRootHandler)
 	// album routes
-	albumRoutes := a.Group("/albums")
+	// albumRoutes := a.Group("/albums")
 
-	albumRoutes.Post("/", h.CreateAlbumHandler)
-	albumRoutes.Get("/", h.GetAlbumsHandler)
-	albumRoutes.Get("/:id", h.GetAlbumHandler)
-	albumRoutes.Put("/:id", h.UpdateAlbumHandlder)
-	albumRoutes.Delete("/:id", h.DeleteAlbumHandler)
+	// albumRoutes.Post("/", h.CreateAlbumHandler)
+	// albumRoutes.Get("/", h.GetAlbumsHandler)
+	// albumRoutes.Get("/:id", h.GetAlbumHandler)
+	// albumRoutes.Put("/:id", h.UpdateAlbumHandlder)
+	// albumRoutes.Delete("/:id", h.DeleteAlbumHandler)
 	
 	// user routes
 	userRoutes := a.Group("/users")
@@ -31,20 +29,20 @@ func LoadRoutes(a *fiber.App, h *handlers.Handler) {
 	userRoutes.Post("/login", h.LoginUserHandler)
 
 	// artist routes
-	artistRoutes := a.Group("/artists")
+	// artistRoutes := a.Group("/artists")
 
-	artistRoutes.Post("/", h.CreateArtistHandler)
-	artistRoutes.Get("/", h.GetArtistsHandler)
-	artistRoutes.Get("/:id", h.GetArtistHandler)
-	artistRoutes.Put("/:id", h.UpdateArtistHandler)
-	artistRoutes.Delete("/:id", h.DeleteArtistHandler)
+	// artistRoutes.Post("/", h.CreateArtistHandler)
+	// artistRoutes.Get("/", h.GetArtistsHandler)
+	// artistRoutes.Get("/:id", h.GetArtistHandler)
+	// artistRoutes.Put("/:id", h.UpdateArtistHandler)
+	// artistRoutes.Delete("/:id", h.DeleteArtistHandler)
 
 	// purchase routes
-	purchaseRoutes := a.Group("/purchases")
-	purchaseRoutes.Use(keyauth.New(keyauth.Config{
-		KeyLookup: "cookie:access-token",
-		Validator: middleware.AuthJwt,
-	}))
-	purchaseRoutes.Post("/", h.CreatePurchaseHandler)
-	purchaseRoutes.Get("/", h.GetPurchasesHandler)
+	// purchaseRoutes := a.Group("/purchases")
+	// purchaseRoutes.Use(keyauth.New(keyauth.Config{
+	// 	KeyLookup: "cookie:access-token",
+	// 	Validator: middleware.AuthJwt,
+	// }))
+	// purchaseRoutes.Post("/", h.CreatePurchaseHandler)
+	// purchaseRoutes.Get("/", h.GetPurchasesHandler)
 }
