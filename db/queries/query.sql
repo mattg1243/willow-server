aa1072764295403743807327677bc2d996f57545
Matt Gallucci <mattgallucci97@gmail.com>
2024-01-05 13:15:47 -0800

    layed out new schemas

--- db/queries/query.sql
+++ db/queries/query.sql
@@ -1,92 +1,133 @@
 -- These queries could and should be split over multiple files
 -- corresponding to each individual table in the db
 
--- Albums
--- name: GetAlbum :one
-SELECT * FROM albums
-WHERE id = $1 LIMIT 1;
-
--- name: ListAlbums :many
-SELECT * FROM albums
-ORDER BY artist;
-
--- name: CreateAlbum :one
-INSERT INTO albums (
-  title, artist, price
-) values (
-  $1, $2, $3
-) RETURNING *;
-
--- name: UpdateAlbum :one
-UPDATE albums
-SET title = $2, artist = $3, price = $4
-WHERE id = $1
-RETURNING *;
-
--- name: DeleteAlbum :exec
-DELETE FROM albums
-WHERE id = $1;
-
 -- Users
 -- name: GetUser :one
 SELECT * FROM users
-WHERE id = $1 LIMIT 1;
-
--- name: GetUsers :many
-SELECT * FROM users
-ORDER BY username;
-
--- name: CreateUser :one
-INSERT INTO users (
-  username, email, balance
-) VALUES (
-  $1, $2, $3
-) RETURNING *;
-
--- name: UpdateUser :one
-UPDATE users
-set username = $2, email = $3, balance = $4
-WHERE id = $1
-RETURNING *;
-
--- name: DeleteUser :exec
-DELETE FROM users
 WHERE id = $1;
 
--- Artists
--- name: GetArtist :one
-SELECT * FROM artists
-WHERE id = $1 LIMIT 1;
+-- Clients
+-- name: CreateClient :one
+-- INSERT INTO clients (
+--   "user", fname, lname, email, rate 
+-- ) VALUES (
+--   $1, $2, $3, $4, $5
+-- ) RETURNING *;
 
--- name: GetArtists :many
-SELECT * FROM artists
-ORDER BY name;
+-- name: GetClient :one
 
--- name: CreateArtist :one
-INSERT INTO artists (
-  name, birthday
-) VALUES (
-  $1, $2
-) RETURNING *;
+-- name: GetClients :many
 
--- name: UpdateArtist :one
-UPDATE artists
-set name = $2, birthday = $3
-WHERE id =$1
-RETURNING *;
+-- name: UpdateClient :one
 
--- name: DeleteArtist :exec
-DELETE from artists
-WHERE id = $1;
+-- name: DeleteClient :exec
 
--- Purchases
--- name: CreatePurchase :one
-INSERT INTO purchases (
-  "user", album, "date"
-) VALUES (
-  $1, $2, $3
-) RETURNING *;
 
--- name: GetPurchases :many
-SELECT * FROM purchases
-ORDER BY id;
+-- Events
+-- name: CreateEvent :one
+
+-- name: GetEvent :one
+
+-- name: GetEvents :many
+
+-- name: UpdateEvent :one
+
+-- name: DeleteEvent :exec
+
+-- Albums
+-- -- name: GetAlbum :one
+-- SELECT * FROM albums
+-- WHERE id = $1 LIMIT 1;
+
+-- -- name: ListAlbums :many
+-- SELECT * FROM albums
+-- ORDER BY artist;
+
+-- -- name: CreateAlbum :one
+-- INSERT INTO albums (
+--   title, artist, price
+-- ) values (
+--   $1, $2, $3
+-- ) RETURNING *;
+
+-- -- name: UpdateAlbum :one
+-- UPDATE albums
+-- SET title = $2, artist = $3, price = $4
+-- WHERE id = $1
+-- RETURNING *;
+
+-- -- name: DeleteAlbum :exec
+-- DELETE FROM albums
+-- WHERE id = $1;
+
+-- -- Users
+-- -- name: GetUser :one
+-- SELECT id, username, email, balance FROM users
+-- WHERE id = $1 LIMIT 1;
+
+-- -- name: GetUserByUsername :one
+-- SELECT id, username, email, balance FROM users
+-- WHERE username = $1 LIMIT 1;
+
+-- -- name: GetUserWithHash :one
+-- SELECT * FROM users
+-- WHERE username = $1 LIMIT 1;
+
+-- -- name: GetUsers :many
+-- SELECT id, username, email, balance FROM users
+-- ORDER BY username;
+
+-- -- name: CreateUser :one
+-- INSERT INTO users (
+--   username, "hash", email, balance
+-- ) VALUES (
+--   $1, $2, $3, $4
+-- ) RETURNING *;
+
+-- -- name: UpdateUser :one
+-- UPDATE users
+-- set username = $2, email = $3, balance = $4
+-- WHERE id = $1
+-- RETURNING *;
+
+-- -- name: DeleteUser :exec
+-- DELETE FROM users
+-- WHERE id = $1;
+
+-- -- Artists
+-- -- name: GetArtist :one
+-- SELECT * FROM artists
+-- WHERE id = $1 LIMIT 1;
+
+-- -- name: GetArtists :many
+-- SELECT * FROM artists
+-- ORDER BY name;
+
+-- -- name: CreateArtist :one
+-- INSERT INTO artists (
+--   name, birthday
+-- ) VALUES (
+--   $1, $2
+-- ) RETURNING *;
+
+-- -- name: UpdateArtist :one
+-- UPDATE artists
+-- set name = $2, birthday = $3
+-- WHERE id =$1
+-- RETURNING *;
+
+-- -- name: DeleteArtist :exec
+-- DELETE from artists
+-- WHERE id = $1;
+
+-- -- Purchases
+-- -- name: CreatePurchase :one
+-- INSERT INTO purchases (
+--   "user", album, "date"
+-- ) VALUES (
+--   $1, $2, $3
+-- ) RETURNING *;
+
+-- -- name: GetPurchases :many
+-- SELECT * FROM purchases
+-- ORDER BY id;
--- db/schemas/schema.sql
+++ db/schemas/schema.sql
@@ -1,29 +1,45 @@
 create table users (
-  id serial primary key,
-  username varchar(128) not null unique,
-  email varchar(128) not null unique,
-  balance integer not null
+	id uuid primary key,
+	username varchar (255) unique not null,
+	fname varchar (50) not null,
+	lname varchar (50) not null,
+	email varchar (255) unique not null,
+	salt text not null,
+	hash text not null,
+	city varchar (255),
+	nameForHeader varchar (255), 
+	phone varchar (255),
+	"state" varchar (255),
+	street varchar (255),
+	zip varchar (255),
+	license varchar (255),
+	paymentInfo json,
+	created_at timestamp not null,
+	updated_at timestamp
 );
 
-create table artists (
-  id serial primary key,
-  "name" varchar(128) unique not null,
-  birthday date not null
+create table clients (
+	id uuid primary key,
+	foreign key("user") references users (id),
+	fname varchar(255) not null,
+	lname varchar(255),
+	email varchar(255),
+	balance int default 0,
+	balanceNotifyThreshold int default 0,
+	rate int not null,
+	isArchived boolean default false,
+	created_at timestamp not null,
+	update_at timestamp
 );
 
-create table albums (
-  id serial primary key,
-  title varchar(128) not null,
-  artist varchar(128) not null,
-  price integer not null,
-  foreign key (artist) references artists (name)
-);
-
-create table purchases (
-  id serial primary key,
-  "user" integer not null,
-  album integer not null,
-  "date" date not null,
-  foreign key ("user") references users (id),
-  foreign key (album) references albums (id)
+create table events (
+	id uuid primary key,
+ 	foreign key (client) references clients (id),
+	date timestamp not null,
+	duration decimal default 0,
+	"type" varchar(255),
+	"detail" text,
+	rate int not null,
+	amount decimal not null,
+	newBalance decimal not null
 );
